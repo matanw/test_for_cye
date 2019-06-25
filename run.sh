c81bfb2882c4139fe2283ba824ed0585bc479ad8
Matan  Wiesner <matan.wiesner@oracle.com>
2019-06-25 21:37:53 +0300

    move to flask restplus

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 virtualenv -p python3 venv
 export FLASK_APP=word_count_service/app.py
 venv/bin/python3 -m pip install flask requests
-venv/bin/python3 -m pip flask run
+venv/bin/python3 -m pip flask run flask_restplus
--- word_count_service/app.py
+++ word_count_service/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restplus import Resource, Api
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+api = Api(app)
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
